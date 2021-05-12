261321fe1376c75d6cae87152bced2f2e2e7ba22
AngelGarcia666 <83615357+AngelGarcia666@users.noreply.github.com>
2021-05-12 07:02:55 +0200

    Cambios Pedidos

--- AmayaPrincipal/Pedidos.Designer.cs
+++ AmayaPrincipal/Pedidos.Designer.cs
@@ -31,6 +31,13 @@ namespace AmayaPrincipal
         {
             this.lblCarrito1 = new System.Windows.Forms.Label();
             this.lblCarrito2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblCarrito1
@@ -38,7 +45,7 @@ namespace AmayaPrincipal
             this.lblCarrito1.AutoSize = true;
             this.lblCarrito1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold);
             this.lblCarrito1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.lblCarrito1.Location = new System.Drawing.Point(252, 26);
+            this.lblCarrito1.Location = new System.Drawing.Point(245, 22);
             this.lblCarrito1.Name = "lblCarrito1";
             this.lblCarrito1.Size = new System.Drawing.Size(90, 30);
             this.lblCarrito1.TabIndex = 0;
@@ -47,12 +54,57 @@ namespace AmayaPrincipal
             // lblCarrito2
             // 
             this.lblCarrito2.AutoSize = true;
-            this.lblCarrito2.Font = new System.Drawing.Font("Ebrima", 12F);
-            this.lblCarrito2.Location = new System.Drawing.Point(182, 148);
+            this.lblCarrito2.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCarrito2.Location = new System.Drawing.Point(35, 94);
             this.lblCarrito2.Name = "lblCarrito2";
-            this.lblCarrito2.Size = new System.Drawing.Size(226, 21);
+            this.lblCarrito2.Size = new System.Drawing.Size(167, 21);
             this.lblCarrito2.TabIndex = 1;
-            this.lblCarrito2.Text = "No hay productos en el pedido.";
+            this.lblCarrito2.Text = " Pedido un Numero :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Font = new System.Drawing.Font("Ebrima", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(39, 173);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(494, 234);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Productos";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(414, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(195, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Ebrima", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(213, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Confirmar ";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Pedidos
             // 
@@ -60,6 +112,10 @@ namespace AmayaPrincipal
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(579, 596);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lblCarrito2);
             this.Controls.Add(this.lblCarrito1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -67,6 +123,8 @@ namespace AmayaPrincipal
             this.Name = "Pedidos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Carrito";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,5 +134,10 @@ namespace AmayaPrincipal
 
         private System.Windows.Forms.Label lblCarrito1;
         private System.Windows.Forms.Label lblCarrito2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
